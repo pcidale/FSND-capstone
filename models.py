8d98fccf3a94282395bbc53df4d53014d0efe88f
Piero <piero@casafari.com>
2020-02-24 03:11:16 +0000

    updated models

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.schema import DropTable
+from sqlalchemy.ext.compiler import compiles
 
 db = SQLAlchemy()
+
+
+@compiles(DropTable, "postgresql")
+def _compile_drop_table(element, compiler, **kwargs):
+    return compiler.visit_drop_table(element) + " CASCADE"
 
 
 def setup_db(app):
